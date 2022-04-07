f43979c4c08b60979a68e348db0faf2f6193d14e
Zaqueu Alves <61361618+Zaqueu-exe@users.noreply.github.com>
2022-04-07 08:35:50 -0300

    Add files via upload

---
+++ secao9_conta_bancaria/src/br/com/alves/entities/Conta.java
@@ -0,0 +1,52 @@
+package br.com.alves.entities;
+
+public class Conta {
+
+    private int account;
+    private String holder;
+    private double saldo;
+
+    public Conta(int account, String titular, double saldo){
+        this.account = account;
+        this.holder = titular;
+        this.depositar(saldo);
+    }
+
+    public Conta(int account, String titular) {
+        this.account = account;
+        this.holder = titular;
+    }
+
+    public int getAccount() {
+        return account;
+    }
+
+    public String getHolder() {
+        return holder;
+    }
+
+    public void setHolder(String holder) {
+        this.holder = holder;
+    }
+
+    public void sacar(double valor){
+        if(valor <0.0 || saldo < valor){
+            throw new IllegalArgumentException("Valor de saque invalido");
+        }
+        saldo -= valor + 5.0;
+    }
+
+    public void depositar(double valor){
+        if(valor <0.0){
+            throw new IllegalArgumentException("Valor de saque invalido");
+        }
+        saldo += valor;
+    }
+
+    @Override
+    public String toString() {
+        return "Account " + account +
+                ", Holder " + holder +
+                ", Balance: $ " + String.format("%.2f", saldo);
+    }
+}
