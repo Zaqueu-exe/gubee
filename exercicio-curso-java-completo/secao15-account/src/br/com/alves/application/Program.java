d5f83ca647d69ac1db6fc150b82b00c11b0dd475
Zaqueu Alves <zaqueu.za15@gmail.com>
2022-04-11 11:45:25 -0300

    Exercicio da secao 15

---
+++ exercicio-curso-java-completo/secao15-account/src/br/com/alves/application/Program.java
@@ -0,0 +1,39 @@
+package br.com.alves.application;
+
+import br.com.alves.application.model.entities.Account;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner scan = new Scanner(System.in);
+
+        try{
+            System.out.println("Enter account data");
+            System.out.print("Number: ");
+            int number = scan.nextInt();
+            System.out.print("Holder: ");
+            scan.nextLine();
+            String holder = scan.nextLine();
+            System.out.print("Initial balance: ");
+            double balance = scan.nextDouble();
+            System.out.print("Withdraw limit: ");
+            double limit = scan.nextDouble();
+
+            Account account = new Account(number, holder, balance, limit);
+
+            System.out.print("Enter amount for withdraw: ");
+            double withdraw = scan.nextDouble();
+            account.withdraw(withdraw);
+
+            System.out.print("New balance: " + String.format("%.2f",account.getBalance()));
+
+        }catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+        }
+        scan.close();
+    }
+}
---
+++ exercicio-curso-java-completo/secao15-account/src/br/com/alves/application/model/entities/Account.java
@@ -0,0 +1,62 @@
+package br.com.alves.application.model.entities;
+
+public class Account {
+    private Integer number;
+    private String holder;
+    private Double balance;
+    private Double withdrawLimit;
+
+    public Account(Integer number, String holder, Double balance, Double withdrawLimit) {
+        if(balance < 0.0 || withdrawLimit < 0.0){
+            throw new IllegalArgumentException("Erro: Valor de saldo inicial ou limite negativo");
+        }
+        this.number = number;
+        this.holder = holder;
+        this.balance = balance;
+        this.withdrawLimit = withdrawLimit;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public String getHolder() {
+        return holder;
+    }
+
+    public void setHolder(String holder) {
+        this.holder = holder;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+
+    public Double getWithdrawLimit() {
+        return withdrawLimit;
+    }
+
+    public void deposit(Double amount){
+        if(amount < 0.0){
+            throw new IllegalArgumentException("Erro: Valor de deposito negativo");
+        }
+        balance += amount;
+    }
+
+    public void withdraw(Double amount){
+        if(amount < 0.0){
+            throw new IllegalArgumentException("Erro: Valor de saque negativo");
+        }else if(amount > this.balance){
+            throw  new IllegalArgumentException("Erro: Valor de Saque maior que o saque disponivel");
+        }
+        else if(amount > this.withdrawLimit){
+            throw  new IllegalArgumentException("Erro: Valor de Saque maior que o limite");
+        }
+        balance -= amount;
+    }
+}
