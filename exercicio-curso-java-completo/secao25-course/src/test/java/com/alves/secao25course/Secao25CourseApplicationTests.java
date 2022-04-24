2e20aed61602d2385723085eef5d3b8327511c02
Zaqueu Alves <zaqueu.za15@gmail.com>
2022-04-24 14:34:49 -0300

    Inicio d projeto springboot

---
+++ exercicio-curso-java-completo/secao25-course/src/test/java/com/alves/secao25course/Secao25CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alves.secao25course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Secao25CourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
